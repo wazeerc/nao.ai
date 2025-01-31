d74462ac43344c3eb5140f9f98896751f97c7969
Wazeer <58399263+wazeerc@users.noreply.github.com>
2025-01-31 20:36:19 +0400

    chore: add reset button

--- components/iLlamaPage.vue
+++ components/iLlamaPage.vue
@@ -1,4 +1,11 @@
 <template>
+  <aside class="inline-flex w-full mb-1 items-end justify-end">
+    <UButton icon="i-heroicons-arrow-path"
+             size="md"
+             color="neutral"
+             variant="ghost"
+             @click="console.log('Reset!')" />
+  </aside>
   <main class="flex flex-col gap-4 p-6 bg-zinc-950 rounded-lg shadow-lg
         border-2 border-zinc-800/40 h-[80vh] w-[80vw] relative">
     <div class="flex w-full">
