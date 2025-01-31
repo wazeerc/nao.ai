c486a980882fec2625031ba3bba54ca013f83aa1
Wazeer <58399263+wazeerc@users.noreply.github.com>
2025-01-31 22:01:58 +0400

    chore: add path aliases

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from 'path';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  alias: {
+    "@": resolve(__dirname, "./"),
+    "@utils": resolve(__dirname, "./utils/index.ts"),
+  },
   compatibilityDate: '2024-11-01',
   css: ['~/assets/css/main.css'],
   devtools: { enabled: false },
-  modules: ['@nuxt/ui'],
+  modules: [
+    '@nuxt/ui',
+    '@pinia/nuxt'
+  ],
   vite: {
     plugins: [
       tailwindcss(),
