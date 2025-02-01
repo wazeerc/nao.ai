b483c0458537e51616fbf74ffa2753c2b20531d8
Wazeer <58399263+wazeerc@users.noreply.github.com>
2025-02-01 19:17:05 +0400

    feat(ui): display responses in markup

--- components/ChatBubble.vue
+++ components/ChatBubble.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
 import { cn } from '@utils';
+import { marked } from 'marked';
 
-defineProps<{
+const props = defineProps<{
   message: string;
   isUser: boolean;
 }>();
+
+const formattedMessage = computed(() => {
+  return !props.isUser ? marked(props.message) : props.message;
+});
 </script>
 
 <template>
-  <span :class="cn('text-zinc-200 shadow-xs rounded-xl px-4 py-2 w-fit',
-    isUser ? 'bg-slate-900' : 'bg-slate-800')">
-    {{ message }}
-  </span>
+  <article :class="cn('text-zinc-200 shadow-xs rounded-xl px-4 py-2 w-fit max-w-full',
+    isUser ? 'bg-slate-900' : 'bg-slate-800 prose prose-invert prose-sm',
+    'prose-p:my-0 prose-ul:my-0 prose-li:my-0 prose-pre:my-0 prose-code:text-zinc-200')"
+           v-html="formattedMessage">
+  </article>
 </template>
