654e638b11428271f0833b9355b53979da73a078
Wazeer <58399263+wazeerc@users.noreply.github.com>
2025-04-12 09:17:25 +0400

    feat(ui): add copy to clipboard btn for long responses

--- components/ChatBubble.vue
+++ components/ChatBubble.vue
@@ -9,6 +9,14 @@ const props = defineProps<{
 }>();
 
 const formattedMessage = computed(() => !props.isUser ? marked(props.message) : props.message);
+
+const hasBeenCopied = ref<boolean>(false);
+const copyToClipboard = async () => {
+  await navigator.clipboard.writeText(props.message);
+  setTimeout(() => {
+    hasBeenCopied.value = true;
+  }, 200);
+};
 </script>
 
 <template>
@@ -20,12 +28,23 @@ const formattedMessage = computed(() => !props.isUser ? marked(props.message) : 
       <span class="text-zinc-300 text-sm">🦙 Llama is thinking...</span>
     </article>
 
-    <article v-else
-             :class="cn(
-              'text-zinc-200 shadow-xs rounded-xl px-4 py-2 max-w-full',
-              isUser ? 'bg-zinc-800 motion-preset-slide-left' : 'bg-slate-800 motion-preset-slide-right prose prose-invert prose-sm',
-              'prose-p:my-0 prose-ul:my-0 prose-li:my-0 prose-pre:my-0 prose-code:text-zinc-200'
-            )"
-             v-html="formattedMessage" />
+    <div v-else
+         class="flex flex-col group">
+      <article :class="cn(
+        'text-zinc-200 shadow-xs rounded-xl px-4 py-2 max-w-full',
+        isUser ? 'bg-zinc-800 motion-preset-slide-left' : 'bg-slate-800 motion-preset-slide-right prose prose-invert prose-sm',
+        'prose-p:my-0 prose-ul:my-0 prose-li:my-0 prose-pre:my-0 prose-code:text-zinc-200'
+      )"
+               v-html="formattedMessage" />
+      <UButton v-if="!isUser && message.length > 1000 && !hasBeenCopied"
+               icon="i-heroicons-clipboard"
+               class="justify-end w-fit ml-auto"
+               :class="cn('mt-1 cursor-pointer motion-preset-pop motion-delay-200',
+                'group-hover:motion-preset-seesaw-lg')"
+               size="xs"
+               color="neutral"
+               variant="ghost"
+               @click.prevent="copyToClipboard" />
+    </div>
   </div>
 </template>
