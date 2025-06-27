26b958bdb35b55f0e3899bac32928308696680b6
wazeerc <58399263+wazeerc@users.noreply.github.com>
2025-06-27 15:10:43 +0400

    feat: implement light mode

---
+++ components/ThemeToggle.vue
@@ -0,0 +1,19 @@
+<script setup>
+const themeStore = useThemeStore();
+</script>
+
+<template>
+  <div class="absolute top-2 right-2 hover:motion-preset-seesaw">
+    <UButton
+      :icon="themeStore.isDark ? 'i-heroicons-sun' : 'i-heroicons-moon'"
+      size="lg"
+      color="neutral"
+      variant="ghost"
+      @click="themeStore.toggleTheme"
+      class="motion-preset-slide-left motion-delay-800"
+      :ui="{
+        base: 'transition-all duration-300'
+      }"
+    />
+  </div>
+</template>
