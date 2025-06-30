adb50d9590cca675ad4cf26c7d3d1982f47521f4
wazeerc <58399263+wazeerc@users.noreply.github.com>
2025-06-30 21:23:10 +0400

    refactor: restructure app ui

--- components/NaoaiPage.vue
+++ components/NaoaiPage.vue
@@ -13,6 +13,22 @@ const model = config.public.ollamaModel;
 </script>
 
 <template>
+  <header>
+    <div class="absolute top-3 left-3 shadow-xs
+           hover:motion-preset-stretch hover:motion-delay-100
+           motion-preset-slide-right motion-delay-800
+           inline-flex items-center justify-center
+           bg-blue-200/80 dark:bg-slate-800/80
+           px-2 py-1
+           rounded-full select-none
+           border border-blue-900/50 dark:border-blue-100/50">
+      <span class="font-semibold text-xs leading-none">beta</span>
+    </div>
+    <ClientOnly>
+      <ThemeToggle />
+    </ClientOnly>
+  </header>
+
   <aside
          class="inline-flex w-full mb-2 items-center justify-between motion-preset-slide-down motion-delay-700">
     <p
@@ -39,4 +55,10 @@ const model = config.public.ollamaModel;
       <ChatInput />
     </div>
   </main>
+
+  <footer class="absolute bottom-3 right-3 text-right text-xs invert opacity-50 font-serif
+                 motion-preset-focus motion-delay-800">
+    © <a href="https://github.com/wazeerc"
+       target="_blank">wazeerc</a> 2025
+  </footer>
 </template>
