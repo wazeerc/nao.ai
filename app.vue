d5f9f7cfed0f5fb26145159a1bffd4930d7309e8
wazeerc <58399263+wazeerc@users.noreply.github.com>
2025-07-01 19:12:11 +0400

    fix: update footer styles for improved visibility

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <ThemeToggle />
   </ClientOnly>
   <NaoaiPage />
-  <footer class="absolute bottom-3 right-3 text-right text-xs text-slate-500 font-serif
+  <footer class="absolute bottom-3 right-3 text-right text-xs invert opacity-50 font-serif
                  motion-preset-focus motion-delay-200">
     © <a href="https://github.com/wazeerc" target="_blank">wazeerc</a> 2025
   </footer>
